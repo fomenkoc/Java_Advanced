40f3a3b48c5fde03750d1d191c9127c1b43b7dac
Alexander Fomenko <fomenkoc@gmail.com>
2021-11-30 11:36:03 +0200

    1

---
+++ src/main/java/homework_01/homework_01/App.java
@@ -0,0 +1,13 @@
+package homework_01.homework_01;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
